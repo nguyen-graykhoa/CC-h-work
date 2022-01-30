587496d58c248f46fec79215f3de0c2607b29ce1
khoa nguyen-gray <khoanguyengray@gmail.com>
2022-01-30 13:12:58 -0800

    create functions per instruction

---
+++ box-it-script/boxit.js
@@ -0,0 +1,11 @@
+const boxIt = (arr) => {}
+
+const drawLine = (length) => {}
+
+const drawTopBorder = (length) => {}
+
+const drawMiddleBorder = (length) => {}
+
+const drawBottomBorder = (length) => {}
+
+const drawBarsAround = (str) => {}
